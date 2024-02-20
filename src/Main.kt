d08185bd66e1ddba47b9652775c2d9b0226a06d4
Shalom-Keesi <shalomkeesi@gmail.com>
2024-02-20 23:32:05 +0300

    added one file

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val Studentname1 ="Deekey Kamau"
+    println(Studentname1
+    )
+    var Age1=(29)
+    println(Age1)
+    var Phonenumber1="0720321336"
+    println(Phonenumber1)
+    var WeightInKg ="75"
+    println (WeightInKg)
+    val citizen =true
+    println(citizen)
+
+
+
+    }
